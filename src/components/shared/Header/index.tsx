ce7dd37e69bead8614b8e028604badcf9cb83f8a
ww_side <daniil.metelia@gmail.com>
2023-08-31 14:38:54 +0300

    refactor(components): remove 'use client' in Header

--- src/components/shared/Header/index.tsx
+++ src/components/shared/Header/index.tsx
@@ -1,13 +1,10 @@
-'use client';
 import { PiPassword } from 'react-icons/pi';
 
-const Header = () => {
+export default function Header() {
   return (
     <header className="flex items-end gap-2 p-3 text-amber-300 font-semibold text-3xl">
       GPass
       <PiPassword />
     </header>
   );
-};
-
-export default Header;
+}
