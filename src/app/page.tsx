92da28d02aa9f8e59e7172a7be0a7de9b38cc2e5
ww_side <daniil.metelia@gmail.com>
2023-08-31 10:50:50 +0300

    feat(page): add home page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+import PasswordOutput from '@/components/shared/PasswordOutput';
+import PasswordSettings from '@/components/shared/PasswordSettings';
+import Button from '@/components/common/Button';
+import React from 'react';
+
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <main className="flex flex-col gap-4 w-8/12 mt-10 mx-auto text-lg">
+      <PasswordOutput />
+      <section className="mx-auto">
+        <Button color="blue">Generate password</Button>
+      </section>
+      <PasswordSettings />
+    </main>
+  );
 }
