6c25c96dca2b8b8538a88cb25fc7a1c301e11790
ww_side <daniil.metelia@gmail.com>
2023-08-31 14:37:38 +0300

    feat(app): add 'Generate Password' button to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
 import PasswordOutput from '@/components/shared/PasswordOutput';
 import PasswordSettings from '@/components/shared/PasswordSettings';
-import Button from '@/components/common/Button';
-import React from 'react';
+import PasswordGenerateBtn from '@/components/shared/PasswordGenerateBtn';
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-4 w-8/12 mt-10 mx-auto text-lg">
+    <main className="flex flex-col gap-4 w-8/12 mt-10 mx-auto text-lg max-md:w-10/12">
       <PasswordOutput />
-      <section className="mx-auto">
-        <Button color="blue">Generate password</Button>
-      </section>
+      <PasswordGenerateBtn />
       <PasswordSettings />
     </main>
   );
