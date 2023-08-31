9fddbe454482676272a0e4a93703322a9fe46832
ww_side <daniil.metelia@gmail.com>
2023-08-31 14:38:04 +0300

    feat(components): add 'background' property to Input

--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -4,14 +4,23 @@ import { ChangeEvent, FC } from 'react';
 interface InputProps {
   type: string;
   value: string;
-  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  background: 'fill' | 'transparent';
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
   blocked?: boolean;
 }
 
-const Input: FC<InputProps> = ({ type, value, onChange, blocked = false }) => {
+const Input: FC<InputProps> = ({
+  type,
+  value,
+  onChange,
+  blocked = false,
+  background,
+}) => {
   return (
     <input
-      className="bg-zinc-600 text-white border-none rounded shadow-md px-3 py-2 focus:outline-none w-full"
+      className={`${
+        background === 'fill' ? 'bg-zinc-600 shadow-md' : 'bg-transparent'
+      } text-white border-none rounded px-3 py-2 focus:outline-none w-full`}
       type={type}
       value={value}
       onChange={onChange}
