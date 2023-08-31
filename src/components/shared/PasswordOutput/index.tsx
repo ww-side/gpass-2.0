4ab2f4e20a0819b54392186dcf548337ef56deac
ww_side <daniil.metelia@gmail.com>
2023-08-31 14:38:41 +0300

    feat(state): add Zustand into password output and settings

--- src/components/shared/PasswordOutput/index.tsx
+++ src/components/shared/PasswordOutput/index.tsx
@@ -1,21 +1,34 @@
 'use client';
-import { ChangeEvent, useState } from 'react';
+import { FC, useState } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import Input from '@/components/common/Input';
+import { usePassword } from '@/store';
+import { GoCopy } from 'react-icons/go';
 
-const PasswordOutput = () => {
-  const [password, setPassword] = useState('Password');
+const PasswordOutput: FC = () => {
+  const password = usePassword(state => state.password);
+  const [isCopied, setIsCopied] = useState(false);
 
-  const handleChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
+  const handleCopy = () => {
+    setIsCopied(true);
+    setTimeout(() => setIsCopied(false), 1000);
   };
 
   return (
-    <Input
-      type="text"
-      value={password}
-      onChange={handleChangePassword}
-      blocked={true}
-    />
+    <section className="flex items-center bg-zinc-600 shadow-md">
+      <Input
+        type="text"
+        value={password}
+        background={'transparent'}
+        blocked={true}
+      />
+      <div className="flex gap-2">
+        {isCopied && <span className="text-white">Copied!</span>}
+        <CopyToClipboard text={password} onCopy={handleCopy}>
+          <GoCopy className="mr-2 cursor-pointer" color="white" size={30} />
+        </CopyToClipboard>
+      </div>
+    </section>
   );
 };
 
--- src/components/shared/PasswordSettings/index.tsx
+++ src/components/shared/PasswordSettings/index.tsx
@@ -1,24 +1,37 @@
 'use client';
-import React, { ChangeEvent, useState } from 'react';
+import { ChangeEvent, FC } from 'react';
 import Input from '@/components/common/Input';
-import Checkbox from '@/components/common/Checkbox';
+import CheckboxList from '@/components/shared/CheckboxList';
+import { usePassword } from '@/store';
 
-const PasswordSettings = () => {
-  const [passwordLength, setPasswordLength] = useState('');
-  const [isUppercase, setIsUppercase] = useState(true);
-  const [isLowercase, setIsLowercase] = useState(true);
-  const [isNumbers, setIsNumbers] = useState(true);
-  const [isSymbols, setIsSymbols] = useState(true);
-
-  const handleSwitchCheckbox = (
-    value: boolean,
-    setValue: React.Dispatch<React.SetStateAction<boolean>>
-  ) => {
-    setValue(!value);
-  };
+const PasswordSettings: FC = () => {
+  const passwordStore = usePassword();
+  const [passwordLength, checkboxes] = usePassword(state => [
+    state.passwordLength,
+    [
+      {
+        propertyKey: 'isUppercase',
+        label: 'Uppercase',
+        checked: state.isUppercase,
+      },
+      {
+        propertyKey: 'isLowercase',
+        label: 'Lowercase',
+        checked: state.isLowercase,
+      },
+      { propertyKey: 'isNumbers', label: 'Numbers', checked: state.isNumbers },
+      { propertyKey: 'isSymbols', label: 'Symbols', checked: state.isSymbols },
+    ],
+  ]);
 
   const handleChangePasswordLength = (e: ChangeEvent<HTMLInputElement>) => {
-    setPasswordLength(e.target.value);
+    passwordStore.set({ passwordLength: e.target.value });
+  };
+
+  const handleSetState = (
+    state: 'isUppercase' | 'isLowercase' | 'isNumbers' | 'isSymbols'
+  ) => {
+    passwordStore.set({ [state]: !passwordStore[state] });
   };
 
   return (
@@ -26,37 +39,17 @@ const PasswordSettings = () => {
       <h2 className="text-center mb-5 font-semibold text-2xl">
         Customize your password
       </h2>
-      <section className="flex gap-40 justify-center">
+      <section className="flex gap-40 justify-center max-md:flex-col max-md:gap-3">
         <div>
           <h3>Password Length</h3>
           <Input
             type="number"
             value={passwordLength}
+            background={'fill'}
             onChange={handleChangePasswordLength}
           />
         </div>
-        <div className="flex flex-col gap-2">
-          <Checkbox
-            label="Uppercase"
-            checked={isUppercase}
-            onChange={() => handleSwitchCheckbox(isUppercase, setIsUppercase)}
-          />
-          <Checkbox
-            label="Lowercase"
-            checked={isLowercase}
-            onChange={() => handleSwitchCheckbox(isLowercase, setIsLowercase)}
-          />
-          <Checkbox
-            label="Numbers"
-            checked={isNumbers}
-            onChange={() => handleSwitchCheckbox(isNumbers, setIsNumbers)}
-          />
-          <Checkbox
-            label="Symbols"
-            checked={isSymbols}
-            onChange={() => handleSwitchCheckbox(isSymbols, setIsSymbols)}
-          />
-        </div>
+        <CheckboxList checkboxes={checkboxes} onToggle={handleSetState} />
       </section>
     </section>
   );
